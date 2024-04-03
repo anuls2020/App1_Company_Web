e249682caa6f7dd28f8b8a18dfd59fb39a0e736c
Owner <anulsasidharan@gmail.com>
2024-04-03 00:17:58 -0400

    Add images, names and roles in the webpage

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import streamlit as st
-
+import pandas as pd
 st.set_page_config(layout="wide")
 
-st.title("The Best Company")
+col1, col2 = st.columns([1, 5])
 
-content = """
-AI startups often emerge from the intersection of technology, data, and entrepreneurship to address various 
-industry challenges or create new opportunities by harnessing the power of AI. They may develop AI software, 
-build AI-powered platforms, create AI algorithms, or design hardware optimized for AI processing
-"""
-st.write(content)
+with col1:
+    st.write("[Home]()")
+    st.write("[Contact Us]()")
 
-st.header("Our Team")
+with col2:
+    st.title("The Best Company")
 
-col1, col2, col3 = st.columns(3)
+    content = """
+    AI startups often emerge from the intersection of technology, data, and entrepreneurship to address various 
+    industry challenges or create new opportunities by harnessing the power of AI. They may develop AI software, 
+    build AI-powered platforms, create AI algorithms, or design hardware optimized for AI processing
+    """
+    st.write(content)
+
+    st.header("Our Team")
+    col3, col4, col5 = st.columns([2, 2, 2])
+
+    # read the csv file
+    df = pd.read_csv("data.csv")
+
+    with col3:
+        for index, row in df[:4].iterrows():
+            name = row["first name"] + " " +row["last name"]
+            st.title(name)
+            st.text(row["role"])
+            st.image("images/"+ row["image"])
+
+    with col4:
+        for index, row in df[4:8].iterrows():
+            name = row["first name"] + " " + row["last name"]
+            st.title(name)
+            st.text(row["role"])
+            st.image("images/" + row["image"])
+
+    with col5:
+        for index, row in df[8:].iterrows():
+            name = row["first name"] + " " + row["last name"]
+            st.title(name)
+            st.text(row["role"])
+            st.image("images/" + row["image"])
